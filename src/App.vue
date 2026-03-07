f173cc7b91f02353aafbb94a90ab4d7f42967c1e
Felipe Falcone <lpmaggots@gmail.com>
2026-03-06 21:40:21 -0300

    feat: initialize Vue 3 project with Vite, Pinia, and Tailwind CSS

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex items-center justify-center h-screen bg-slate-900">
+    <h1 class="text-4xl font-bold text-red-400 underline">
+      Vue 3 Project
+    </h1>
+  </div>
+</template>
+
+<style scoped></style>
