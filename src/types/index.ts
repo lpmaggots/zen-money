2c2cc91956a7dc8647cbda5830191631dba21df3
Felipe Falcone <lpmaggots@gmail.com>
2026-03-30 16:35:09 -0300

    refactor: move category constants import to the correct module

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,4 @@ export type {
   TransactionFormDialogData,
 } from './transaction'
 
-export { CATEGORY_LABELS, INCOME_CATEGORIES, EXPENSE_CATEGORIES } from '@/utils/categories'
-
 export type { FilterType, FilterState } from './filters'
